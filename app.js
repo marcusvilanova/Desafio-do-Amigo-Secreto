ad952e6e12b37478a7780cd5a0ecff5a73eda880
Marcus Vila Nova <marcusviniciusvilanovasilva@gmail.com>
2025-02-12 05:16:46 -0300

    Receber dados de nomes de amigos

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function addAmigos () {
+    let inputField = document.getElementById("amigoInput");
+    let nomeAmigo = inputField.value.trim();
+
+    if(nomeAmigo ==="") {
+        alert("Por favor, insira um nome válido");
+        return;
+    }
+
+    amigos.push(nomeAmigo);
+    let friendItem = document.createElement("li");
+    friendItem.textContent = friendName;
+    friendsContainer.appendChild(friendItem);
+    
+    inputField.value = ""; // Limpar o campo de entrada
+} 
+
